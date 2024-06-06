8aa0035331f09617e1e85174b459c352fcb609df
Pawel Jaworski Software <jpsoftware@MacBook-Pro-Pawel.local>
2024-06-06 23:58:20 +0200

    tokenize

--- lexer.ts
+++ lexer.ts
@@ -35,3 +35,57 @@ function isint(str: string) {
 	const bounds = ["0".charCodeAt(0), "9".charCodeAt(0)];
 	return c >= bounds[0] && c <= bounds[1];
 }
+
+export function tokenize(sourceCode: string): Token[] {
+	const tokens = new Array<Token>();
+	const src = sourceCode.split("");
+
+	while (src.length > 0) {
+		if (src[0] == "(") {
+			tokens.push(token(src.shift(), TokenType.OpenParen));
+		} else if (src[0] == ")") {
+			tokens.push(token(src.shift(), TokenType.CloseParen));
+		}
+		else if (src[0] == "+" || src[0] == "-" || src[0] == "*" || src[0] == "/") {
+			tokens.push(token(src.shift(), TokenType.BinaryOperator));
+		}
+		else if (src[0] == "=") {
+			tokens.push(token(src.shift(), TokenType.Equals));
+		}
+		else {
+			if (isint(src[0])) {
+				let num = "";
+				while (src.length > 0 && isint(src[0])) {
+					num += src.shift();
+				}
+
+				tokens.push(token(num, TokenType.Number));
+			}
+			else if (isalpha(src[0])) {
+				let ident = "";
+				while (src.length > 0 && isalpha(src[0])) {
+					ident += src.shift();
+				}
+
+				const reserved = KEYWORDS[ident];
+				if (reserved) {
+					tokens.push(token(ident, reserved));
+				} else {
+					tokens.push(token(ident, TokenType.Identifier));
+				}
+			} else if (isskippable(src[0])) {
+				src.shift();
+			} 
+			else {
+				console.error(
+					"Unreconized character found in source: ",
+					src[0].charCodeAt(0),
+					src[0]
+				);
+				Deno.exit(1);
+			}
+		}
+	}
+
+	return tokens;
+}
